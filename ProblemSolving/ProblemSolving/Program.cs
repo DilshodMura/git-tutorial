9e87e96a0441ec7c821adfd4bd95631853c1985c
DilshodMura <75676999+DilshodMura@users.noreply.github.com>
2021-01-02 07:29:25 -0600

    Fibonacci sequence

--- ProblemSolving/ProblemSolving/Program.cs
+++ ProblemSolving/ProblemSolving/Program.cs
@@ -4,9 +4,42 @@ namespace ProblemSolving
 {
     class Program
     {
+
+        //static void Fibonacci(int a)
+        //{
+            
+           
+        //}
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("please enter your nubmer...");
+            int input1 = int.Parse(Console.ReadLine());
+            int a = 0;
+            int b = 1;
+            int c;
+            
+            for (int i = 0;; i++)
+            {
+                c = a + b;
+                a = c - a;
+                b = c;
+
+                if (input1 == c)
+                {
+                    Console.WriteLine("Fibonacci");
+                    break;
+                }
+                else if(input1<c)
+                {
+                    Console.WriteLine("не фибоначчи");
+                    break;
+                }
+              
+            }
+            
+
         }
+
     }
-}
+    }
+
