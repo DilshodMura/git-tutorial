b39a8fcccb55888ffd6435014dd1ade796433a72
Rustam Umarov <rustam.umarov@yahoo.com>
2021-01-28 11:20:33 -0600

    added test initialize

--- ProblemSolving.Tests/EasyTasksTests.cs
+++ ProblemSolving.Tests/EasyTasksTests.cs
@@ -7,112 +7,117 @@ namespace ProblemSolving.Tests
     [TestClass]
     public class EasyTasksTests
     {
-        EasyTasks easyTasks = new EasyTasks();
-        int input; 
+        //class level
+        private EasyTasks _easyTasks;
+        
+        // Initializes on call of every test method
+        [TestInitialize]
         public void Initialize()
         {
-            easyTasks.IsFibonacci(input);
-            bool isFibonacci = Convert.ToBoolean(input);
-            Assert.IsTrue(isFibonacci);
+            _easyTasks = new EasyTasks();
+            //method level
         }
+
         [TestMethod]
         public void IsFibonacciHappyPath_13()
         {
-            input = 13; 
-            Initialize();
+            int input = 13; 
+            var isFibonacci = _easyTasks.IsFibonacci(input);
+            Assert.IsTrue(isFibonacci);
         }
+
         [TestMethod]
         public void IsFibonacciHappyPath_55()
         {
             var input = 55;
-            var easyTasks = new EasyTasks(); //creating an instance of EasyTasks
-            bool isFibonacci = easyTasks.IsFibonacci(input); //calling method using instance of EasyTasks
+            bool isFibonacci = _easyTasks.IsFibonacci(input); //calling method using instance of EasyTasks
             Assert.IsTrue(isFibonacci);
         }
+
         [TestMethod]
         public void IsFibonacciHappyPath_21()
         {
             var input = 21;
-            var easyTasks = new EasyTasks(); //creating an instance of EasyTasks
-            bool isFibonacci = easyTasks.IsFibonacci(input); //calling method using instance of EasyTasks
+            bool isFibonacci = _easyTasks.IsFibonacci(input); //calling method using instance of EasyTasks
             Assert.IsTrue(isFibonacci);
         }
+
         [TestMethod]
         public void IsFibonacciFailPath_14()
         {
             var input = 14;
-            var easyTasks = new EasyTasks(); //creating an instance of EasyTasks
-            bool isFibonacci = easyTasks.IsFibonacci(input); //calling method using instance of EasyTasks
+            bool isFibonacci = _easyTasks.IsFibonacci(input); //calling method using instance of EasyTasks
             Assert.IsFalse(isFibonacci);
         }
+
         [TestMethod]
         public void IsFibonacciFailPath_12()
         {
             var input = 12;
-            var easyTasks = new EasyTasks(); //creating an instance of EasyTasks
-            bool isFibonacci = easyTasks.IsFibonacci(input); //calling method using instance of EasyTasks
+            bool isFibonacci = _easyTasks.IsFibonacci(input); //calling method using instance of EasyTasks
             Assert.IsFalse(isFibonacci);
         }
+
         [TestMethod]
         public void IsFibonacciFailPath_10()
         {
             var input = 10;
-            var easyTasks = new EasyTasks(); //creating an instance of EasyTasks
-            bool isFibonacci = easyTasks.IsFibonacci(input); //calling method using instance of EasyTasks
+            bool isFibonacci = _easyTasks.IsFibonacci(input); //calling method using instance of EasyTasks
             Assert.IsFalse(isFibonacci);
         }
+
         [TestMethod]
         public void IsSuminputHappyPath()
         {
             var presetValueOne = 15;
             var presetValueTwo = 5;
-            var easyTasks = new EasyTasks();
-            int isSuminputHappyPath = easyTasks.SumOfTwo(presetValueOne, presetValueTwo);
+            int isSuminputHappyPath = _easyTasks.SumOfTwo(presetValueOne, presetValueTwo);
             Assert.AreEqual(20, isSuminputHappyPath);
         }
+
         [TestMethod]
         public void IsSuminputHappyPath_2()
         {
             var presetValueOne = 16;
             var presetValueTwo = 30;
-            var easyTasks = new EasyTasks();
-            int isSuminputHappyPath = easyTasks.SumOfTwo(presetValueOne, presetValueTwo);
+            int isSuminputHappyPath = _easyTasks.SumOfTwo(presetValueOne, presetValueTwo);
             Assert.AreEqual(46, isSuminputHappyPath);
         }
+
         [TestMethod]
         public void IsSuminputHappyPath_3()
         {
             var presetValueOne = 195;
             var presetValueTwo = 200;
             var easyTasks = new EasyTasks();
-            int isSuminputHappyPath = easyTasks.SumOfTwo(presetValueOne, presetValueTwo);
+            int isSuminputHappyPath = _easyTasks.SumOfTwo(presetValueOne, presetValueTwo);
             Assert.AreEqual(395, isSuminputHappyPath);
         }
+
         [TestMethod]
         public void IsSumInputFailPath_1()
         {
             var presetValueOne = 10;
             var presentValueTwo = 5;
-            var easyTasks = new EasyTasks();
-            int failPath = easyTasks.SumOfTwo(presetValueOne, presentValueTwo);
+            int failPath = _easyTasks.SumOfTwo(presetValueOne, presentValueTwo);
             Assert.AreNotEqual(25, failPath);
         }
+
         [TestMethod]
         public void IsSumInputFailPath_2()
         {
             var presetValueOne = 15;
             var presentValueTwo = 5;
-            var easyTasks = new EasyTasks();
-            int failPath = easyTasks.SumOfTwo(presetValueOne, presentValueTwo);
+            int failPath = _easyTasks.SumOfTwo(presetValueOne, presentValueTwo);
             Assert.AreNotEqual(25, failPath);
         }
+
         [TestMethod]
         public void IsSumInputFailPath_3()
         {
             var presetValueOne = 13;
             var presentValueTwo = 6;
-            var easyTasks = new EasyTasks();
-            int failPath = easyTasks.SumOfTwo(presetValueOne, presentValueTwo);
+            int failPath = _easyTasks.SumOfTwo(presetValueOne, presentValueTwo);
             Assert.AreNotEqual(25, failPath);
         }
     }
