9dc35cd9516ccc29f31545061656d94ff32c7636
Rustam Umarov <rustam.umarov@yahoo.com>
2021-01-28 11:39:52 -0600

    solution instructions FooBuzz

--- ProblemSolving.Core/EasyTasks.cs
+++ ProblemSolving.Core/EasyTasks.cs
@@ -29,5 +29,29 @@ namespace ProblemSolving.Core
         {
             return firstInput + secondInput;
         }
+
+        //foreach
+        10 = 1, 2, 3, 4 ... 10
+        //for(int i = 100; i--; i > 10){ array[i]; i++;}
+
+
+
+        public List<string> FooBuzz(List<int> inputNumbers){
+            // return list of strings that would contain the result
+            // divided by 2 - Foo
+            // divided by 3 - Buzz
+            // divided by both 2 && 3 - FooBuzz 
+            // not divded by any 2 or 3 - FooBuzzBar
+
+            // loop through list of integers and append respective string
+            // to the list of string and return 
+            // input example {1, 2, 3, 4, 5, 6}
+            // return example: {"Foo", "Buzz", "FooBuzzBar", "Buzz"}
+
+            // TODO: 
+            // Solution:
+
+        }
+
     }
 }
